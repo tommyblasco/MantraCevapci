a3a9b2068aa6eeec162690c6560b3e6e7e755236
Thomas <sacchinit@gmail.com>
2022-12-29 19:30:01 +0100

    Add files via upload

--- pages/01_ Squadre.py
+++ pages/01_ Squadre.py
@@ -3,6 +3,7 @@ from datetime import date
 from PIL import Image
 import requests
 from io import BytesIO
+import plotly.graph_objects as go
 from funzioni import *
 
 st.title("Squadre")
@@ -15,6 +16,13 @@ def load_images(team):
     url_ds = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/persone/"+team+" - ds.jpg".replace(' ', '%20')
     url_mister = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/persone/"+team+" - mister.jpg".replace(' ', '%20')
     return [url_stemma, url_maglie, url_pres, url_ds, url_mister]
+@st.cache
+def load_images_cup():
+    url_camp = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/cups/Campionato.png"
+    url_luk = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/cups/Coppa%20Luk.png"
+    url_iva = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/cups/Supercoppa%20Ivanica.png"
+    url_ulmi = "https://raw.githubusercontent.com/tommyblasco/MantraCevapci/main/images/cups/Ulmi.png"
+    return [url_camp, url_luk, url_iva, url_ulmi]
 
 list_team=tuple(set([x for x in mercato['A'] if str(x) != 'nan']))
 
@@ -41,7 +49,7 @@ with tab1:
             st.image(Image.open(BytesIO(requests.get(load_images(team=sel_team)[3]).content)),caption='DS')
         with col5:
             st.image(Image.open(BytesIO(requests.get(load_images(team=sel_team)[4]).content)),caption='Mister')
-
+    st.header("Rosa attuale")
     st.dataframe(rosa)
 
     with st.expander("Giocatori in prestito"):
@@ -59,9 +67,29 @@ with tab1:
         else:
             st.write("Nessun giocatore attualmente in primavera")
 with tab2:
-    with st.expander("Palmares"):
-        st.write("...")
-    with st.expander("Cronistoria"):
-        st.write("...")
+    col6, col7, col8, col9 = st.columns(4)
+    with col6:
+        his_camp=cronistoria(team=sel_team,comp="MantraCevapci")
+        st.image(Image.open(BytesIO(requests.get(load_images_cup()[0]).content)), caption='Titoli: '+str(his_camp[his_camp['Risultato']=='Vincitore'].shape[0]))
+        st.dataframe(his_camp)
+    with col7:
+        his_luk = cronistoria(team=sel_team, comp="Coppa Luk")
+        st.image(Image.open(BytesIO(requests.get(load_images_cup()[1]).content)), caption='Titoli: '+str(his_luk[his_luk['Risultato']=='Vincitore'].shape[0]))
+        st.dataframe(his_luk)
+    with col8:
+        his_iva = cronistoria(team=sel_team, comp="Supercoppa Ivanica")
+        st.image(Image.open(BytesIO(requests.get(load_images_cup()[2]).content)), caption='Titoli: '+str(his_iva[his_iva['Risultato']=='Vincitore'].shape[0]))
+        if his_iva.shape[0]>0:
+            st.dataframe(his_iva)
+    with col9:
+        his_ulmi = cronistoria(team=sel_team, comp="ULMI")
+        st.image(Image.open(BytesIO(requests.get(load_images_cup()[3]).content)), caption='Titoli: '+str(his_ulmi[his_ulmi['Risultato']=='Vincitore'].shape[0]))
+        if his_ulmi.shape[0] > 0:
+            st.dataframe(his_ulmi)
 with tab3:
-    st.write("...") #grafici
+    n_rosa = go.Figure(go.Indicator( domain={'x': [0, 1], 'y': [0, 1]},
+        value=rosa.shape[0], mode="gauge+number", title={'text': "Giocatori in rosa"},
+        gauge={'axis': {'range': [None, 40]}, 'steps': [
+                   {'range': [0, rosa.shape[0]-primav_players(team=sel_team).shape[0]], 'color': "lightgray"},
+                   {'range': [rosa.shape[0]-primav_players(team=sel_team).shape[0], rosa.shape[0]], 'color': "gray"}]}))
+    st.plotly_chart(n_rosa)
