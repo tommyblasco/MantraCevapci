92bcd3984c173ad134c6dbfab1ed20b0f2d3ae08
Thom Sax <sacchinit@gmail.com>
2023-04-09 10:36:26 +0200

    gioc piu odiati fix2

--- funzioni.py
+++ funzioni.py
@@ -98,8 +98,8 @@ def voti_arricchiti():
 def voti_arricchiti_opp():
     home_camp = campionato[['Stagione', 'Giornata', 'Home','Away']]
     away_camp = campionato[['Stagione', 'Giornata', 'Away','Home']]
-    home_camp.rename({'Home':'Squadra','Away':'Opponent','Giornata':'Day'},inplace=True)
-    away_camp.rename({'Home':'Squadra','Away':'Opponent','Giornata':'Day'},inplace=True)
+    home_camp.rename(columns={'Home':'Squadra','Away':'Opponent','Giornata':'Day'},inplace=True)
+    away_camp.rename(columns={'Home':'Squadra','Away':'Opponent','Giornata':'Day'},inplace=True)
     df = home_camp.append(away_camp)
     df['Giornata']=[x+2 for x in df['Day']]
     df = df.drop('Day', axis=1)
